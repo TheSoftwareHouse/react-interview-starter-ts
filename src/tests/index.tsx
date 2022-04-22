8ca1c896afea1293c11e526cce6390a20dbdd664
cezarybak <cezary.bak0@gmail.com>
2022-04-22 16:33:35 +0200

    feat: update package-lock

--- src/tests/index.tsx
+++ src/tests/index.tsx
@@ -1,24 +1,32 @@
 // see https://testing-library.com/docs/react-testing-library/setup#custom-render
-import React, { ReactNode } from 'react';
-import { MemoryRouter as Router } from 'react-router-dom';
-import { render, RenderOptions, RenderResult } from '@testing-library/react';
-import { Queries } from '@testing-library/dom';
+import React, { ReactNode } from "react";
+import { MemoryRouter as Router } from "react-router-dom";
+import { render, RenderOptions, RenderResult } from "@testing-library/react";
+import { Queries } from "@testing-library/dom";
 
 const Wrapper = ({ children }: { children?: ReactNode }) => {
   return <Router>{children}</Router>;
 };
 
-function customRender(ui: React.ReactElement, options?: Omit<RenderOptions, 'queries'>): RenderResult;
-function customRender<Q extends Queries>(ui: React.ReactElement, options: RenderOptions<Q>): RenderResult<Q>;
+function customRender(
+  ui: React.ReactElement,
+  options?: Omit<RenderOptions, "queries">
+): RenderResult;
+
 function customRender<Q extends Queries>(
   ui: React.ReactElement,
-  options?: RenderOptions<Q> | Omit<RenderOptions, 'queries'>,
+  options: RenderOptions<Q>
+): RenderResult<Q>;
+
+function customRender<Q extends Queries>(
+  ui: React.ReactElement,
+  options?: RenderOptions<Q> | Omit<RenderOptions, "queries">
 ): RenderResult<Q> | RenderResult {
   return render<Q>(ui, { wrapper: options?.wrapper ?? Wrapper, ...options });
 }
 
 // re-export everything
-export * from '@testing-library/react';
+export * from "@testing-library/react";
 
 // override render method
 export { customRender as render };
